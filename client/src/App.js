b191feca18d87ed6e6b8273bb4143fd44051c116
Lance Walker <LWalk2678@msn.com>
2021-11-15 15:41:10 -0700

    coded routes in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { useState, useEffect } from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom'
 
+import Home from './screens/Home/Home'
 import Smoothies from './screens/Smoothies/Smoothies'
 import SmoothieCreate from './screens/SmoothieCreate/SmoothieCreate'
 import SmoothieEdit from './screens/SmoothieEdit/SmoothieEdit'
@@ -13,43 +14,45 @@ import SignIn from './screens/SignIn/SignIn'
 import SignOut from './screens/SignOut/SignOut'
 
 const App = () => {
-  const [user, setUser] = useState(null)
+  // const [user, setUser] = useState(null)
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      const user = await verifyUser()
-      user ? setUser(user) : setUser(null)
-    }
-    fetchUser()
-  }, [])
+  // useEffect(() => {
+  //   const fetchUser = async () => {
+  //     const user = await verifyUser()
+  //     user ? setUser(user) : setUser(null)
+  //   }
+  //   fetchUser()
+  // }, [])
 
 
   return (
     <div className="App">
       <Switch>
         <Route exact path="/">
-          <Home user={user} />
+          <Home/>
         </Route>
         <Route path="/sign-up">
-          <SignUp setUser={setUser} />
+          <SignUp/>
         </Route>
         <Route path="/sign-in">
-          <SignIn setUser={setUser} />
+          <SignIn/>
         </Route>
         <Route path="/sign-out">
-          <SignOut setUser={setUser} />
+          <SignOut/>
         </Route>
         <Route exact path="/smoothies">
-          <Smoothies user={user} />
+          <Smoothies/>
         </Route>
         <Route path="/add-smoothie">
-          {user ? <SmoothieCreate user={user} /> : <Redirect to="/sign-up" />}
+          <SmoothieCreate/>
+          {/* {user ? <SmoothieCreate/> : <Redirect to="/sign-up" />} */}
         </Route>
         <Route exact path="/smoothies/:id/edit">
-          {user ? <SmoothieEdit user={user} /> : <Redirect to='/' />}
+          <SmoothieEdit/>
+          {/* {user ? <SmoothieEdit/> : <Redirect to='/' />} */}
         </Route>
         <Route exact path="/smoothies/:id">
-          <SmoothieDetail user={user} />
+          <SmoothieDetail/>
         </Route>
       </Switch>
     </div>
---
+++ client/src/screens/SignIn/Signin.js
@@ -0,0 +1,11 @@
+
+const SignIn = (props) => {
+ 
+  return (
+    <div>
+        <h1>Sign In Page</h1>
+      </div>
+  )
+}
+
+export default SignIn
