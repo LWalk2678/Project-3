4d9a422c1898a73f3d5674a004b926b939a45c63
Clint Gunter <clint@clint.io>
2021-11-16 10:40:55 -0600

    Add smoothies code

--- client/src/screens/Smoothies/Smoothies.js
+++ client/src/screens/Smoothies/Smoothies.js
@@ -1,10 +1,32 @@
-import { useState, useEffect } from 'react';
-import './Smoothies.css';
+import { useState, useEffect } from 'react'
+import './Smoothies.css'
 
-import { Layout, Product, Search } from '../../components';
-import { getSmoothies } from '../../services/smoothies';
+import { Layout, Product, Search } from '../../components'
+import { getSmoothies } from '../../services/smoothies'
 
 const Smoothies = (props) => {
+  const [smoothies, setSmoothies] = useState([])
+  const [searchResult, setSearchResult] = useState([])
+
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const allSmoothies = await getSmoothies()
+      setSmoothies(allSmoothies)
+      setSearchResult(allSmoothies)
+    }
+    fetchSmoothies()
+  }, [])
+
+  const handleSearch = (event) => {
+    const results = smoothies.filter((smoothie) =>
+      smoothie.name.toLowerCase().includes(event.target.value.toLowerCase())
+    )
+    setSearchResult(results)
+    setApplySort(true)
+  }
+
+  const handleSubmit = (event) => event.preventDefault()
+
   return (
     <div>
       <h1>Smoothies Page</h1>
