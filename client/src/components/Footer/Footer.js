89f410c2907bb23075911da84eb8658a16250d7a
Terra <bclaycamp@gmail.com>
2021-11-16 09:47:04 -0500

    made footer and base to smoothieCard/smoothieCards

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Footer(props) {
+  return (
+    <div className="footer">
+      <p>
+        We were talking about Norway, Sweden and Finland etc, and I mentioned
+        that I knew a Norwegian girl called Hedda. Girlfriend promptly replies
+        with 'funny, I know a guy from there called "footer"'.
+      </p>
+    </div>
+  );
+}
