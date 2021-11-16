88110e128e77992345a4a5e08151b828fb45609e
Lance Walker <LWalk2678@msn.com>
2021-11-16 09:44:33 -0700

    removed sort from index.js

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,4 +2,4 @@ import Layout from './Layout/Layout'
 import Smoothie from './Smoothie/Smoothie'
 import Search from './Search/Search'
 import SmoothieCards from './SmoothieCards/SmoothieCards'
-export { Layout, Smoothie, Search, Sort, SmoothieCards }
+export { Layout, Smoothie, Search, SmoothieCards }
