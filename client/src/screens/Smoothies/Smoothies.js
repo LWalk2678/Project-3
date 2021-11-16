9c48fb054645150fe10b36e7a7a88947c720ec69
Clint Gunter <clint@clint.io>
2021-11-16 09:58:20 -0600

    Add imports

--- client/src/screens/Smoothies/Smoothies.js
+++ client/src/screens/Smoothies/Smoothies.js
@@ -1,3 +1,8 @@
+import { useState, useEffect } from 'react';
+import './Smoothies.css';
+
+import { Layout, Product, Search } from '../../components';
+import { getSmoothies } from '../../services/smoothies';
 
 const Smoothies = (props) => {
   return (
