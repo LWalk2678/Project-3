d5a43861fd31c8df7e7d943fed5f44cfde9b2316
Lance Walker <LWalk2678@msn.com>
2021-11-16 09:33:32 -0700

    coded components-index.js

---
+++ client/src/components/index.js
@@ -0,0 +1,6 @@
+import Layout from './Layout/Layout'
+import Smoothie from './Smoothie/Smoothie'
+import Search from './Search/Search'
+import Sort from './Sort/Sort'
+import SmoothieCards from './SmoothieCards/SmoothieCards'
+export { Layout, Smoothie, Search, Sort, SmoothieCards }
