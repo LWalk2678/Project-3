eabf2f6743fd09282277b42c7f3910c771ed47e1
Clint Gunter <clint@clint.io>
2021-11-17 11:19:19 -0600

    Revise Layout and Home

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -2,14 +2,16 @@ import './Layout.css'
 import Nav from '../Nav/Nav'
 import Footer from '../Footer/Footer'
 
-const Layout = (props) => (
-    <div className='layout'>
-        <Nav user={props.user} />
-        <main>
-            {props.children}
-        </main>
-        <Footer />
-    </div>
-)
+const Layout = (props) => {
+    return (
+        <div className='layout'>
+            <Nav user={props.user} />
+            <main>
+                {props.children}
+            </main>
+            <Footer />
+        </div>
+    )
+}
 
 export default Layout
