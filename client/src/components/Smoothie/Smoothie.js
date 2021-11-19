13c64b694b151cdf8d74f743beb629d96052822b
Lance Walker <LWalk2678@msn.com>
2021-11-19 13:49:07 -0700

    CSS on Smoothies page

--- client/src/components/Smoothie/Smoothie.js
+++ client/src/components/Smoothie/Smoothie.js
@@ -5,7 +5,7 @@ const Smoothie = (props)=> {
     return (
         <div>
             <Link className="smoothie" to={`/smoothies/${props._id}`}>
-                <img className="smoothie-image" src={props.imageURL} alt={props.title} />
+                <div className='smoothie-image-container'><img className="smoothie-image" src={props.imageURL} alt={props.title} /></div>
                 <div className="smoothie-title">{props.title}</div>
                 <div className="price">{`$${props.price}`}</div>
             </Link>
